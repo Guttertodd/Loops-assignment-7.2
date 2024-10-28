3160545ef0f70e79881a927776bb80e0cfaddd21
339713273 <339713273@2260I-L29-026>
2024-10-28 09:37:13 -0400

    Almost done

--- Loops assignment 7.2/Program.cs
+++ Loops assignment 7.2/Program.cs
@@ -15,18 +15,33 @@
             //}
             //Console.WriteLine("Blast Off");
 
-            Console.WriteLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine("X    Y");
+            //for (int i = -10; i <= 10; i = i + 2)
+
+            //{
+            //    Console.WriteLine(i + " " + " " + i * i);
+            //}
 
 
-            for (int y = -10; y <= 10; y =y + 2)
-            {
-                Console.WriteLine(y + ". ");
-            }
+            string aldworth, name;
+            Console.WriteLine("Give me a name and i'll type it 10 times!");
+            name = Console.ReadLine();
+            aldworth = "5";
           
-                
+            for (int i = 1; i <= 10; i = i + 1)
+               
+            {
+                Console.WriteLine(i + ". " + name);              
+            }
+            if (name == "Aldworth") 
+            {
+                Console.WriteLine(i <= 5);
+            }
             }
         }
             }
-        }
+        
     
 
